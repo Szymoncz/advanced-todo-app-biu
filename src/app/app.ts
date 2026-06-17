42a1453a0e826188ec816ec9d0f4f612b114b92e
Szymon Czudowski <sz.czudowski@gmail.com>
2026-06-17 23:30:11 +0200

    Mobileview added and RWD fixes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,13 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { map } from 'rxjs/operators';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-root',
@@ -14,4 +17,16 @@ import { MatListModule } from '@angular/material/list';
 })
 export class App {
   protected readonly title = signal('advanced-todo-app');
+
+  private breakpointObserver = inject(BreakpointObserver);
+  isMobile = toSignal(this.breakpointObserver.observe(Breakpoints.Handset).pipe(map(result => result.matches)),
+    { initialValue: false }
+);
+
+sidenavOpened = signal(true);
+
+  toggleSidenav() {
+    this.sidenavOpened.update(v => !v);
+  }
+
 }
