9445c54846f49b1cdd028c3dbb729c3f0f4f3df5
Szymon Czudowski <sz.czudowski@gmail.com>
2026-07-14 00:53:44 +0200

    Interceptor for token added in user session - preapring for login functionaliy feature

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,17 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter, withRouterConfig } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+import { authInterceptor } from './core/interceptors/auth-interceptor';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes, withRouterConfig({ onSameUrlNavigation: 'reload' })),
-    provideHttpClient(),
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([authInterceptor])),
     provideNativeDateAdapter(),
     provideCharts(withDefaultRegisterables())
   ]
