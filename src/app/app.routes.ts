21afb42c46cfc3490314986bbde8baad5a0e2c5e
Szymon Czudowski <sz.czudowski@gmail.com>
2026-07-14 16:09:46 +0200

    Added auth and example login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth-guard';
 
 export const routes: Routes = [
     {
@@ -8,25 +9,34 @@ export const routes: Routes = [
     },
     {
         path: 'dashboard',
+        canActivate: [authGuard],
         loadComponent: () => import('./features/dashboard/dashboard')
         .then(m => m.Dashboard)
     },
     {
         path: 'tasks',
+        canActivate: [authGuard],
         loadComponent: () => import('./features/tasks/task-list/task-list')
         .then(m => m.TaskList)
     },
      
     {
         path: 'tasks/:id',
+        canActivate: [authGuard],
         loadComponent: () => import('./features/tasks/task-detail/task-detail')
         .then(m => m.TaskDetail)
     },
    {
   path: 'users',
+  canActivate: [authGuard],
   loadComponent: () =>
     import('./features/users')
       .then(m => m.Users)
+},
+{
+    path: 'login',
+    loadComponent: () => import('./features/auth/login')
+    .then(m => m.Login)
 },
 
   {
