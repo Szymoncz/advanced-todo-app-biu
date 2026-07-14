9ac32a726fe4993ca51b7c4f0cadc0f7e042dc4c
Szymon Czudowski <sz.czudowski@gmail.com>
2026-07-14 18:33:23 +0200

    Login page corrected and added validation

--- src/app/features/auth/login.ts
+++ src/app/features/auth/login.ts
@@ -1,9 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule} from '@angular/material/input';
+import { MatButtonModule} from '@angular/material/button';
+import { MatIconModule} from '@angular/material/icon';
+import { Auth } from '../../core/services/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export class Login {}
+export class Login {
+  private fb = inject(FormBuilder);
+  private authService = inject(Auth);
+
+  form = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  });
+
+  error = '';
+  loading = false;
+  hidePassword = true;
+
+  submit() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    this.loading = true;
+    this. error = '';
+
+    this.authService.login(this.form.value as any).subscribe({
+      next: () => {
+        this.loading = false;
+      },
+      error: (err) => {
+        this.loading = false;
+        this.error = 'Nieprawidłowy email lub hasło';
+      }
+    });
+  }
+}
